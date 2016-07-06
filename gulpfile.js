b1165f3043823f282d995913c4a241382f7d08ca
应开翔 <rubykaikai@gmail.com>
2016-07-07 07:36:26 +0800

    update pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 /**
  *  @author 应开翔
  *  @update 2016-7-1
+ *  @update 2016-7-7
+ *    添加图片压缩
  *  @Email: rubykaikai@gmail.com
  *  @QQ: 540719103
  */
@@ -9,6 +11,8 @@ var gulp       = require('gulp');
 var htmlConcat = require('gulp-content-includer');
 var livereload = require('gulp-livereload');
 var sass       = require('gulp-ruby-sass');
+var imagemin   = require('gulp-imagemin');
+var cache      = require('gulp-cache');
 
 // 说明
 gulp.task('help', function(){
@@ -18,31 +22,43 @@ gulp.task('help', function(){
 });
 
 // 调试
-gulp.task('dev', ['reload', 'sass', 'html']);
+gulp.task('dev', ['reload', 'sass', 'html', 'imagemin']);
 
 // 自动刷新
 gulp.task('reload', function(){
 	livereload.listen();
 	gulp.watch('./src/**/*.*',function(file){
-        livereload.changed(file.path);
-    });
+      livereload.changed(file.path);
+  });
 });
 
 // 编译sass
 gulp.task('sass', function(){
 	gulp.watch('src/sass/*.scss', function(file){
 		return sass('src/sass/*.scss', {style:'expanded'})
-        	.pipe(gulp.dest('dist/css/'));
+      .pipe(gulp.dest('dist/css/'));
 	});
 });
 
 // 合并html
 gulp.task('html', function(){
-    gulp.watch('src/*.html', function(){
-        gulp.src('src/*.html')
-            .pipe(htmlConcat({
-                includerReg:/<!\-\-include\s+"([^"]+)"\-\->/g
-            }))
-            .pipe(gulp.dest('dist/'))
-    });
+  gulp.watch('src/*.html', function(){
+    gulp.src('src/*.html')
+      .pipe(htmlConcat({
+        includerReg:/<!\-\-include\s+"([^"]+)"\-\->/g
+      }))
+      .pipe(gulp.dest('dist/'))
+  });
+});
+
+// 图片压缩
+gulp.task('imagemin', function(){
+  gulp.watch('./src/**/*.*',function(file){
+    gulp.src('./src/img/*.{png,jpg,gif,ico}')
+      .pipe(cache(imagemin({
+        optimizationLevel:5,
+        progressive: true
+      })))
+      .pipe(gulp.dest('dist/img/'))
+  });
 });
